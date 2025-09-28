4038fc8b60b959aac7175f3248306cd296a27864
newlandsvalley <john.watson@gmx.co.uk>
2025-09-28 12:36:03 +0100

    Amend contact us page
    Use my name, and fix bug in my email address!

--- src/TuneBank/HTML/ContactUs.purs
+++ src/TuneBank/HTML/ContactUs.purs
@@ -2,6 +2,7 @@ module TuneBank.HTML.ContactUs (contactUs) where
 
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
+import TuneBank.HTML.Utils (css)
 
 contactUs :: forall i p. HH.HTML i p
 contactUs =
@@ -10,10 +11,13 @@ contactUs =
     [ HH.h2_
         [ HH.text "Please get in touch" ]
     , HH.p_
-        [ HH.text "Perhaps you have questions about the site or suggestions for improvements. If so, "
-        , HH.a
-            [ HP.href "mailto:john.watson@@gmx.co.uk" ]
-            [ HH.text "contact us" ]
-        , HH.text " and we'd be delighted to hear from you."
+        [ HH.text "Perhaps you have questions about the site or suggestions for improvements. If so, contact "
+        , HH.address            
+            [ css "contactus"]
+            [ HH.a
+              [ HP.href "mailto:john.watson@gmx.co.uk" ]
+              [ HH.text "John Watson" ]
+            ]
+        , HH.text " and I'd be delighted to hear from you."
         ]
     ]
